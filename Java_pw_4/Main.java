03f9c1df64f7d3cec91b78979b8456eb469517fe
Cyrill <kirillp001100@gmail.com>
2025-09-22 12:01:07 +0300

    1.0

---
+++ Java_pw_4/Main.java
@@ -0,0 +1,16 @@
+interface BasicSoldier {
+    void get_full_name();
+    void get_age();
+}
+
+class Soldier implements BasicSoldier {
+    @Override
+    void get_age
+}
+
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
