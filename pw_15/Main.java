888cb93d34a34d99362c6c279b53e49a449255fd
Cyrill <kirillp001100@gmail.com>
2025-11-27 11:59:00 +0300

    delete useless line

--- pw_15/Main.java
+++ pw_15/Main.java
@@ -6,7 +6,6 @@ public class Main {
         
         securitySystem.addSecurityEventListener(alarm);
         
-        System.out.println("=== Тестирование системы безопасности ===");
         securitySystem.detectIntruder("главный вход");
         securitySystem.detectIntruder("задний двор");
         securitySystem.detectIntruder("окно на первом этаже");
